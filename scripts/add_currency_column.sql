f1ebe99e5219b2376880808e7495688623159faf
LeDat0312 <dat.vanle.0312@gmail.com>
2025-11-17 01:30:40 +0700

    Clean up scripts folder - remove old migrations and debug scripts

--- scripts/add_currency_column.sql
+++
@@ -1,5 +0,0 @@
--- Add currency column to accounts table if not exists
-ALTER TABLE accounts ADD COLUMN IF NOT EXISTS currency VARCHAR(10) DEFAULT 'USD';
-
--- Update existing accounts to have USD as default currency if NULL
-UPDATE accounts SET currency = 'USD' WHERE currency IS NULL;
